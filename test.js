94de124b70a9ae60f181b3fa7b6ea750f8707ab1
Seonghun Kang <gceohoony@gmail.com>
2022-02-25 10:36:45 +0900

    function to find even numbers

---
+++ test.js
@@ -0,0 +1,11 @@
+
+const Function Test (arr => {
+    const result = [];
+    for(let i=0; i < arr.length; i++) {
+        if(arr[i] / 2 === 0) {
+            result.push(arr[i])
+        }
+    }
+
+    return result; 
+});
